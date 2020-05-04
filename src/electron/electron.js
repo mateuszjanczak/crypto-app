10fd739a180142140ed86668ac8648465c1bac32
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-05-04 22:05:16 +0200

    Initial electron configuration

---
+++ src/electron/electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+function createWindow () {
+
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL(
+        isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+
+    win.setMenu(null);
+
+    win.setMinimumSize(800, 600);
+
+    win.webContents.openDevTools();
+
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+});
